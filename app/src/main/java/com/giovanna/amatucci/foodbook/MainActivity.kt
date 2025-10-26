1d997969e4361ad0c3330284c1ccce73f5e6264e
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2025-10-26 19:00:00 -0300

    style(theme): Apply FoodBookTheme at the root level

--- app/src/main/java/com/giovanna/amatucci/foodbook/MainActivity.kt
+++ app/src/main/java/com/giovanna/amatucci/foodbook/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.giovanna.amatucci.foodbook.presentation.navigation.AppNavHost
 import com.giovanna.amatucci.foodbook.ui.theme.FoodBookTheme
@@ -18,13 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FoodBookTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
                     val navController = rememberNavController()
                     AppNavHost(navController)
                 }
-            }
         }
     }
 }
