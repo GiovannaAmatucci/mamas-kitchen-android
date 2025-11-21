f410e64f50daf2618b3b1e2850cd2b29c7f26abc
Giovanna Amatucci <giovannaamatucci@hotmail.com>
2025-11-21 17:01:13 -0300

    feat(ui): add animated splash screen and handle configuration changes

---
+++ app/src/main/java/com/giovanna/amatucci/foodbook/presentation/navigation/content/SplashScreen.kt
@@ -0,0 +1,63 @@
+package com.giovanna.amatucci.foodbook.presentation.navigation.content
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.giovanna.amatucci.foodbook.R
+import com.giovanna.amatucci.foodbook.util.constants.UiConstants
+import kotlinx.coroutines.delay
+
+@Composable
+fun AnimatedSplashScreen(onAnimationFinished: () -> Unit) {
+    var startAnimation by remember { mutableStateOf(true) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) UiConstants.SPLASH_SCREEN_IF_START_ANIMATION else UiConstants.SPLASH_SCREEN_ELSE_START_ANIMATION,
+        animationSpec = tween(durationMillis = UiConstants.SPLASH_SCREEN_DURATION_ANIMATION)
+    )
+    LaunchedEffect(key1 = true) {
+        delay(UiConstants.SPLASH_SCREEN_DELAY)
+        onAnimationFinished()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.alpha(alphaAnim.value)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_mamas_kitchen_logo_light),
+                contentDescription = stringResource(R.string.navigation_mamas_kitchen_content_description_logo)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_mamas_kitchen_text_light),
+                contentDescription = stringResource(R.string.navigation_mamas_kitchen_content_description)
+            )
+        }
+    }
+}
--- app/src/main/java/com/giovanna/amatucci/foodbook/ui/theme/Color.kt
+++ app/src/main/java/com/giovanna/amatucci/foodbook/ui/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 /**
  * @param PrimaryLight A cor principal da marca. Usada para componentes de destaque como botões de ação, seleção ativa, indicadores de progresso.
  */
-val PrimaryLight = Color(0xFFC00018)
+val PrimaryLight = Color(0xFFFEAB55)
 
 /**
  * @param OnPrimaryLight Cor para o texto e ícones exibidos SOBRE a cor [PrimaryLight]. Garante contraste e legibilidade.
@@ -15,17 +15,17 @@ val OnPrimaryLight = Color(0xFFFFFFFF)
 /**
  * @param PrimaryContainerLight Uma cor de tom mais leve da cor primária. Usada para containers com menor destaque, como tags, caixas de diálogo ou fundos de cabeçalhos menos proeminentes.
  */
-val PrimaryContainerLight = Color(0xFFFFDAD6)
+val PrimaryContainerLight = Color(0xFFFFDDB7)
 
 /**
  * @param OnPrimaryContainerLight Cor para o texto e ícones exibidos SOBRE a cor [PrimaryContainerLight]. Garante contraste e legibilidade.
  */
-val OnPrimaryContainerLight = Color(0xFF410003)
+val OnPrimaryContainerLight = Color(0xFF2B1700)
 
 /**
  * @param SecondaryLight Uma cor secundária da marca. Usada para realçar seções, filtros, ou elementos com menor importância que a primária, mas ainda relevantes.
  */
-val SecondaryLight = Color(0xFF556253)
+val SecondaryLight = Color(0xFF705C41)
 
 /**
  * @param OnSecondaryLight Cor para o texto e ícones exibidos SOBRE a cor [SecondaryLight]. Garante contraste e legibilidade.
@@ -35,17 +35,17 @@ val OnSecondaryLight = Color(0xFFFFFFFF)
 /**
  * @param SecondaryContainerLight Uma cor de tom mais leve da cor secundária. Usada para containers secundários, como chips de seleção inativos ou fundos de cartões alternativos.
  */
-val SecondaryContainerLight = Color(0xFFD9E7D3)
+val SecondaryContainerLight = Color(0xFFFADFAC)
 
 /**
  * @param OnSecondaryContainerLight Cor para o texto e ícones exibidos SOBRE a cor [SecondaryContainerLight]. Garante contraste e legibilidade.
  */
-val OnSecondaryContainerLight = Color(0xFF131F12)
+val OnSecondaryContainerLight = Color(0xFF281A05)
 
 /**
  * @param TertiaryLight Uma cor terciária (acento). Usada para destacar elementos pontuais, como barras de progresso sutis, textos de destaque ou elementos gráficos.
  */
-val TertiaryLight = Color(0xFF006A60)
+val TertiaryLight = Color(0xFF54643E)
 
 /**
  * @param OnTertiaryLight Cor para o texto e ícones exibidos SOBRE a cor [TertiaryLight]. Garante contraste e legibilidade.
@@ -55,12 +55,12 @@ val OnTertiaryLight = Color(0xFFFFFFFF)
 /**
  * @param TertiaryContainerLight Uma cor de tom mais leve da cor terciária. Usada para containers de acento ou pequenos elementos de destaque.
  */
-val TertiaryContainerLight = Color(0xFF74F8E5)
+val TertiaryContainerLight = Color(0xFFD8EABC)
 
 /**
  * @param OnTertiaryContainerLight Cor para o texto e ícones exibidos SOBRE a cor [TertiaryContainerLight]. Garante contraste e legibilidade.
  */
-val OnTertiaryContainerLight = Color(0xFF00201C)
+val OnTertiaryContainerLight = Color(0xFF131F03)
 
 /**
  * @param ErrorLight Cor para indicar estados de erro. Usada para mensagens de erro, ícones de validação e bordas de campos inválidos.
@@ -85,37 +85,37 @@ val OnErrorContainerLight = Color(0xFF410002)
 /**
  * @param BackgroundLight A cor de fundo principal da tela.
  */
-val BackgroundLight = Color(0xFFFCFDF7)
+val BackgroundLight = Color(0xFFFFFBFF)
 
 /**
  * @param OnBackgroundLight Cor para o texto e ícones exibidos SOBRE a cor [BackgroundLight].
  */
-val OnBackgroundLight = Color(0xFF1A1C19)
+val OnBackgroundLight = Color(0xFF1F1B16)
 
 /**
  * @param SurfaceLight A cor principal para superfícies de componentes como Cards, BottomSheets, Menus e Dialogs.
  */
-val SurfaceLight = Color(0xFFE4E4D5)
+val SurfaceLight = Color(0xFFFBEAD5)
 
 /**
  * @param OnSurfaceLight Cor para o texto e ícones exibidos SOBRE a cor [SurfaceLight].
  */
-val OnSurfaceLight = Color(0xFF1A1C19)
+val OnSurfaceLight = Color(0xFF1F1B16)
 
 /**
  * @param SurfaceVariantLight Uma cor de superfície alternativa, geralmente mais escura ou com tonalidade diferente. Usada para caixas de pesquisa, divisores ou outros elementos que precisam se destacar da superfície principal.
  */
-val SurfaceVariantLight = Color(0xFFE4E4D5)
+val SurfaceVariantLight = Color(0xFFF0E0CF)
 
 /**
  * @param OnSurfaceVariantLight Cor para o texto e ícones exibidos SOBRE a cor [SurfaceVariantLight].
  */
-val OnSurfaceVariantLight = Color(0xFF45483D)
+val OnSurfaceVariantLight = Color(0xFF4E4539)
 
 /**
  * @param OutlineLight Cor para bordas finas, contornos de campos de texto inativos ou divisores.
  */
-val OutlineLight = Color(0xFF76786C)
+val OutlineLight = Color(0xFF807567)
 
 /**
  * @param ScrimLight Uma cor translúcida usada para sobreposições, como fundos de modais, para escurecer o conteúdo abaixo.
@@ -125,17 +125,17 @@ val ScrimLight = Color(0xFF000000)
 /**
  * @param InverseSurfaceLight Uma superfície de cor contrastante que inverte a relação normal de Surface/OnSurface. Útil para tooltips em temas claros ou para destacar partes da UI.
  */
-val InverseSurfaceLight = Color(0xFF2F312D)
+val InverseSurfaceLight = Color(0xFF35302A)
 
 /**
  * @param InverseOnSurfaceLight Cor para o texto e ícones exibidos SOBRE a cor [InverseSurfaceLight].
  */
-val InverseOnSurfaceLight = Color(0xFFF1F2E9)
+val InverseOnSurfaceLight = Color(0xFFF9EFE7)
 
 /**
  * @param InversePrimaryLight Uma cor primária contrastante, usada em conjunto com [InverseSurfaceLight] para manter a legibilidade da marca.
  */
-val InversePrimaryLight = Color(0xFFC00018)
+val InversePrimaryLight = Color(0xFFFFB961)
 
 
 // Paleta Gerada para o Tema Escuro (Dark Theme) - Os usos são os mesmos do tema claro, mas as cores são adaptadas para o contexto escuro.
@@ -143,62 +143,62 @@ val InversePrimaryLight = Color(0xFFC00018)
 /**
  * @param PrimaryDark A cor principal da marca para o tema escuro. Usada para componentes de destaque como botões de ação, seleção ativa, indicadores de progresso.
  */
-val PrimaryDark = Color(0xFFFFB3AC)
+val PrimaryDark = Color(0xFFFFB961)
 
 /**
  * @param OnPrimaryDark Cor para o texto e ícones exibidos SOBRE a cor [PrimaryDark]. Garante contraste e legibilidade.
  */
-val OnPrimaryDark = Color(0xFF680008)
+val OnPrimaryDark = Color(0xFF472A00)
 
 /**
  * @param PrimaryContainerDark Uma cor de tom mais escura da cor primária. Usada para containers com menor destaque, como tags, caixas de diálogo ou fundos de cabeçalhos menos proeminentes.
  */
-val PrimaryContainerDark = Color(0xFF93000F)
+val PrimaryContainerDark = Color(0xFF663E00)
 
 /**
  * @param OnPrimaryContainerDark Cor para o texto e ícones exibidos SOBRE a cor [PrimaryContainerDark]. Garante contraste e legibilidade.
  */
-val OnPrimaryContainerDark = Color(0xFFFFDAD6)
+val OnPrimaryContainerDark = Color(0xFFFFDDB7)
 
 /**
  * @param SecondaryDark Uma cor secundária da marca para o tema escuro. Usada para realçar seções, filtros, ou elementos com menor importância que a primária, mas ainda relevantes.
  */
-val SecondaryDark = Color(0xFFBDCBA8)
+val SecondaryDark = Color(0xFFDEC392)
 
 /**
  * @param OnSecondaryDark Cor para o texto e ícones exibidos SOBRE a cor [SecondaryDark]. Garante contraste e legibilidade.
  */
-val OnSecondaryDark = Color(0xFF283426)
+val OnSecondaryDark = Color(0xFF3F2E17)
 
 /**
  * @param SecondaryContainerDark Uma cor de tom mais escura da cor secundária. Usada para containers secundários, como chips de seleção inativos ou fundos de cartões alternativos.
  */
-val SecondaryContainerDark = Color(0xFF3E4A3B)
+val SecondaryContainerDark = Color(0xFF57442B)
 
 /**
  * @param OnSecondaryContainerDark Cor para o texto e ícones exibidos SOBRE a cor [SecondaryContainerDark]. Garante contraste e legibilidade.
  */
-val OnSecondaryContainerDark = Color(0xFFD9E7D3)
+val OnSecondaryContainerDark = Color(0xFFFADFAC)
 
 /**
  * @param TertiaryDark Uma cor terciária (acento) para o tema escuro. Usada para destacar elementos pontuais, como barras de progresso sutis, textos de destaque ou elementos gráficos.
  */
-val TertiaryDark = Color(0xFF53DBC9)
+val TertiaryDark = Color(0xFFBCCEA2)
 
 /**
  * @param OnTertiaryDark Cor para o texto e ícones exibidos SOBRE a cor [TertiaryDark]. Garante contraste e legibilidade.
  */
-val OnTertiaryDark = Color(0xFF003731)
+val OnTertiaryDark = Color(0xFF273514)
 
 /**
  * @param TertiaryContainerDark Uma cor de tom mais escura da cor terciária. Usada para containers de acento ou pequenos elementos de destaque.
  */
-val TertiaryContainerDark = Color(0xFF005048)
+val TertiaryContainerDark = Color(0xFF3D4C28)
 
 /**
  * @param OnTertiaryContainerDark Cor para o texto e ícones exibidos SOBRE a cor [TertiaryContainerDark]. Garante contraste e legibilidade.
  */
-val OnTertiaryContainerDark = Color(0xFF74F8E5)
+val OnTertiaryContainerDark = Color(0xFFD8EABC)
 
 /**
  * @param ErrorDark Cor para indicar estados de erro no tema escuro. Usada para mensagens de erro, ícones de validação e bordas de campos inválidos.
@@ -223,37 +223,37 @@ val OnErrorContainerDark = Color(0xFFFFDAD6)
 /**
  * @param BackgroundDark A cor de fundo principal da tela para o tema escuro.
  */
-val BackgroundDark = Color(0xF0000000)
+val BackgroundDark = Color(0xFF000000)
 
 /**
  * @param OnBackgroundDark Cor para o texto e ícones exibidos SOBRE a cor [BackgroundDark].
  */
-val OnBackgroundDark = Color(0xFFE2E3DD)
+val OnBackgroundDark = Color(0xFFEAE1D9)
 
 /**
  * @param SurfaceDark A cor principal para superfícies de componentes como Cards, BottomSheets, Menus e Dialogs no tema escuro.
  */
-val SurfaceDark = Color(0xFF1A1C19)
+val SurfaceDark = Color(0xFF1F1B16)
 
 /**
  * @param OnSurfaceDark Cor para o texto e ícones exibidos SOBRE a cor [SurfaceDark].
  */
-val OnSurfaceDark = Color(0xFFE2E3DD)
+val OnSurfaceDark = Color(0xFFEAE1D9)
 
 /**
  * @param SurfaceVariantDark Uma cor de superfície alternativa, geralmente mais clara ou com tonalidade diferente. Usada para caixas de pesquisa, divisores ou outros elementos que precisam se destacar da superfície principal no tema escuro.
  */
-val SurfaceVariantDark = Color(0xFF45483D)
+val SurfaceVariantDark = Color(0xFF4E4539)
 
 /**
  * @param OnSurfaceVariantDark Cor para o texto e ícones exibidos SOBRE a cor [SurfaceVariantDark].
  */
-val OnSurfaceVariantDark = Color(0xFFC5C8B9)
+val OnSurfaceVariantDark = Color(0xFFD2C4B4)
 
 /**
  * @param OutlineDark Cor para bordas finas, contornos de campos de texto inativos ou divisores no tema escuro.
  */
-val OutlineDark = Color(0xFF8F9285)
+val OutlineDark = Color(0xFF9A8F80)
 
 /**
  * @param ScrimDark Uma cor translúcida usada para sobreposições, como fundos de modais, para escurecer o conteúdo abaixo no tema escuro.
@@ -263,14 +263,14 @@ val ScrimDark = Color(0xFF000000)
 /**
  * @param InverseSurfaceDark Uma superfície de cor contrastante que inverte a relação normal de Surface/OnSurface. Útil para tooltips em temas escuros ou para destacar partes da UI.
  */
-val InverseSurfaceDark = Color(0xFFE2E3DD)
+val InverseSurfaceDark = Color(0xFFEAE1D9)
 
 /**
  * @param InverseOnSurfaceDark Cor para o texto e ícones exibidos SOBRE a cor [InverseSurfaceDark].
  */
-val InverseOnSurfaceDark = Color(0xFF1A1C19)
+val InverseOnSurfaceDark = Color(0xFF1F1B16)
 
 /**
  * @param InversePrimaryDark Uma cor primária contrastante, usada em conjunto com [InverseSurfaceDark] para manter a legibilidade da marca no tema escuro.
  */
-val InversePrimaryDark = Color(0xFFC00018)
+val InversePrimaryDark = Color(0xFF865300)
